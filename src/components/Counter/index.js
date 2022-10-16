6dc1bda4d01932d02d5479c7e4728e335550e993
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-10-16 10:23:26 +0800

    feat: 添加 useEffect 的测试

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+
+  function handleAlertClick() {
+    setTimeout(() => {
+      alert('you clicked on: ' + count);
+    }, 3000)
+  }
+
+  useEffect(() => {
+    document.title = `you clicked ${count} times`
+    setTimeout(() => {
+      console.log('count = ', count);
+    }, 3000)
+  })
 
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={ () => setCount(count + 1) }>
         Click
+      </button>
+      <button onClick={ handleAlertClick }>
+        Alert
       </button>
     </div>
   )
